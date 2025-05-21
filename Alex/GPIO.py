b2f35b1ad29a096c9dad98396cfc5aa4d6ebb247
Arne Holzke <arneholzke@gmail.com>
2025-05-21 12:04:23 +0200

    com3

--- Alex/GPIO.py
+++ Alex/GPIO.py
@@ -3,7 +3,9 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(20,GPIO.OUT)
-x = 1
+#GPIO.setup(16,GPIO.OUT)
+GPIO.setup(5,GPIO.IN)
+""" x = 1
 y = 0.1
 while x <=100:
     GPIO.output(20,GPIO.HIGH)
@@ -11,5 +13,11 @@ while x <=100:
     GPIO.output(20,GPIO.LOW)
     time.sleep(y)
     x = x + 1
-    y = y + 0.1
+    y = y + 0.1 """
+while True:
+    if GPIO.input(5)==0:
+        GPIO.output(20,GPIO.LOW)
+    else:
+        GPIO.output(20,GPIO.HIGH)
+
 
