e981657559cf46d3994915efb9c1a37f1cef5546
Arne Holzke <arneholzke@gmail.com>
2025-05-21 13:35:53 +0200

    com4

---
+++ Test.py
@@ -0,0 +1,13 @@
+""" import sys
+print(sys.version) """
+
+""" x, y, z = "5", 5, 5.0
+print([x,y,z])
+#print(type(range([x,y,z]))) """
+
+# plante = ["apple", "banana", "cherry"]
+
+# x, y, z = plante
+# print(x)
+# print(y)
+# print(z)
