585d1d16837301211a4dbbe124e993a19e2b17a3
Arne Holzke <arneholzke@gmail.com>
2025-05-23 09:03:56 +0200

    com6

--- Alex/GPIO_1_2_Aufgaben.py
+++ Alex/GPIO_1_2_Aufgaben.py
@@ -16,34 +16,38 @@ GPIO.setup(18,GPIO.IN)
 GPIO.setup(22,GPIO.OUT)
 data = dht11.DHT11(pin=18)
 
-
-while True:
-    #if GPIO.input(26) == 1:
-          #GPIO.output(16,GPIO.HIGH)
-          #GPIO.output(6,GPIO.HIGH)
-    #else:
-        #GPIO.output(16,GPIO.LOW) 
-        #GPIO.output(6,GPIO.LOW)
-    while GPIO.input(19) == 1:
-            GPIO.output(20,GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(20,GPIO.LOW)
-            time.sleep(0.5)
-    while GPIO.input(13) == 1:
-            GPIO.output(21,GPIO.HIGH)
-            time.sleep(0.5)
-            GPIO.output(21,GPIO.LOW)
-            time.sleep(0.5)
-    indoor = data.read()
-    if indoor.is_valid():   
-          print ("temperatur:" + str(indoor.temperature)) 
-          print("Luftfeuchtigkeit:" + str(indoor.humidity))
-          x = indoor.temperature
-          if x >= 25.0:
-              GPIO.output(22,GPIO.HIGH)
-          else:
-              GPIO.output(22,GPIO.LOW)
-GPIO.cleanup()
+try:
+    while True:
+        #Beleuchtung(Aufgabe1)
+        if GPIO.input(26) == 1:
+            GPIO.output(16,GPIO.HIGH)
+            GPIO.output(6,GPIO.HIGH)
+        else:
+            GPIO.output(16,GPIO.LOW) 
+            GPIO.output(6,GPIO.LOW)
+        #Blinker(Aufgabe2)
+        while GPIO.input(19) == 1:
+                GPIO.output(20,GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(20,GPIO.LOW)
+                time.sleep(0.5)
+        while GPIO.input(13) == 1:
+                GPIO.output(21,GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(21,GPIO.LOW)
+                time.sleep(0.5)
+        #Temperatursensor(Aufgabe3)
+        indoor = data.read()
+        if indoor.is_valid():   
+            print ("temperatur:" + str(indoor.temperature)) 
+            print("Luftfeuchtigkeit:" + str(indoor.humidity))
+            x = indoor.temperature
+            if x >= 25.0:
+                GPIO.output(22,GPIO.HIGH)
+            else:
+                GPIO.output(22,GPIO.LOW)
+except KeyboardInterrupt:        
+    GPIO.cleanup()
 
 
 
